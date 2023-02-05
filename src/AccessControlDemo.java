bff4cd1d9278c74a459732e2b12feb708b5363e5
Mohana Venkatraman <mohanadesi@gmail.com>
2023-02-05 20:32:15 +0530

    Access modifier example section 5 1.5
    Core java section 5 1.5

---
+++ src/AccessControlDemo.java
@@ -0,0 +1,21 @@
+
+public class AccessControlDemo {
+	int i;
+	public int j;
+	private int k;
+	//to access private variable use setter and getter method
+	void setK(int a)
+	{
+		k=a;
+	}
+	int getK()
+	{
+		return k;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+// related program AccessControlTest.java
---
+++ src/AccessControlTest.java
@@ -0,0 +1,17 @@
+
+public class AccessControlTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccessControlDemo obj1 = new AccessControlDemo();
+		obj1.i=10;
+		obj1.j=20;
+		obj1.setK(30);
+		System.out.println(obj1.i);
+		System.out.println(obj1.j);
+		System.out.println(obj1.getK());
+	}
+
+}
+
+//AccessControlDemo related program
