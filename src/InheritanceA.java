b6d4610acbe5ab91ec592da73f005c3996204e69
Mohana Venkatraman <mohanadesi@gmail.com>
2023-02-05 21:57:10 +0530

    Inheritance section 5 1.13
    corejava section 5 1.13

---
+++ src/InheritanceA.java
@@ -0,0 +1,12 @@
+
+public class InheritanceA {
+int i;
+int j;
+void showij()
+{
+	System.out.println("VAlue of i " +i);
+	System.out.println("VAlue of j " +j);
+	//System.out.println("Values of i and j are " +i  +j);
+}
+}
+//InheritanceB and InheritanceDemo
---
+++ src/InheritanceB.java
@@ -0,0 +1,10 @@
+
+public class InheritanceB extends InheritanceA{
+	int k;
+	void showk()
+	{
+		System.out.println("VAlue of k " +k);
+	}
+
+}
+//InheritanceA and InheritanceDemo
