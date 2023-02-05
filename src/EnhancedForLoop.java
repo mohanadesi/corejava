3a918dfc372bda6ffc5afbaa1215f3196702717f
Mohana Venkatraman <mohanadesi@gmail.com>
2023-02-05 16:07:26 +0530

    Enhanced for loop with arrays Section 5 , 1.9
    Corejava Section 5 , 1.9

---
+++ src/EnhancedForLoop.java
@@ -0,0 +1,17 @@
+public class EnhancedForLoop 
+{  
+public static void main(String[] args) 
+{  
+     
+    int arr[]={12,23,44,56,78};  
+    String arrs[] = {"Mohana","Kavin"};
+    for(int i:arr)
+    {  
+        System.out.println(i);  
+    }
+    for(String s:arrs)
+    {
+    	System.out.println(s);
+    }
+}  
+} 
